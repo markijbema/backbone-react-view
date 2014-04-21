615451b5f756ed86ff7cae86a71c29701959095b
Mark IJbema <markijbema@gmail.com>
2014-04-21 18:07:42 +0200

    Initial version of Backbone React View

---
+++ backbone_react_view.js
@@ -0,0 +1,7 @@
+class window.BackboneReactView extends Backbone.View
+  tagName: 'span'
+  initialize: (options) ->
+    throw "You haven't defined component, silly developer" unless options.component
+    @on 'show', ->
+      React.renderComponent(@options.component, @el)
+
